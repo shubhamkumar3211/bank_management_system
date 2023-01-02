87c83da9238cac834599b4375929048934112a3f
shubhamkumar3211 <121149423+shubhamkumar3211@users.noreply.github.com>
2023-01-02 21:52:16 +0530

    Add files via upload

---
+++ main _sql.sql
@@ -0,0 +1,10 @@
+create database bank_management;
+use bank_management;
+create table Account (Name varchar(25),Account_No varchar(10),DOB varchar(10),Address varchar(30),contact_no bigint,opening_bal bigint);
+show tables;
+create table Amount(Name varchar(25),Account_No varchar(10),Balance int);
+show tables;
+desc Account;
+desc Amount;
+select*from bank_management.Account;
+
